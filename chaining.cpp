3fca590743824d23724486ce330e31cb8533efcf
Dabhi Nikul <125889984+Nikul-63@users.noreply.github.com>
2023-03-22 20:42:16 +0530

    Add files via upload

---
+++ chaining.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+ int main()
+ {
+ 	float d=44.4;
+ 	long double e=44.4;
+ 	
+ 	cout<<"The value of d is:-"<<d<<endl<<"The value of e is:-"<<e;
+ 	return 0;
+ }
