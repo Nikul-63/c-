0f5ebef8c06f9b9bfa458f020bd83ca15c013263
Dabhi Nikul <125889984+Nikul-63@users.noreply.github.com>
2023-03-22 20:44:59 +0530

    Add files via upload

---
+++ typecasting.cpp
@@ -0,0 +1,17 @@
+/* Type casting :- type casting is a way of changing on expression from one data type to another. */
+
+#include<iostream>
+using namespace std;
+
+ int main()
+ {
+ 	int a=40;
+ 	float b=50.60;
+ 	
+ 	cout<<"The value of a is:-"<<(float)a<<endl;
+ 	cout<<"The value of a is:-"<<float(a)<<endl;
+ 	
+ 	cout<<"The value of b is:-"<<(int)b<<endl;
+ 	cout<<"The value of b is:-"<<int(b)<<endl;
+ 	return 0;
+ }
